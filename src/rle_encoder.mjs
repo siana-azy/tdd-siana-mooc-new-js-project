acdf66c7eedca01e2892d15e318622f2766429de
Anais <anaisriviere03@gmail.com>
2026-06-14 14:20:42 +0400

    rle encoder multiple cells on the same row return the count and the cell

--- src/rle_encoder.mjs
+++ src/rle_encoder.mjs
@@ -1,5 +1,20 @@
 export function encoderRLE(cells) {
     if(cells.size === 0)return "!";
+
+    const manyx = [];
+    for(const cell of cells){
+        const [x, y] = cell.split(",").map(Number);
+        manyx.push(x);
+    } 
+    manyx.sort((a,b)=> a - b);
+
+    const count = manyx.length;
+
+    if(count > 1){
+        return count + "o!";
+    }
+
     return "o!";
+
   
 }
--- test/rle_encoder.test.mjs
+++ test/rle_encoder.test.mjs
@@ -3,11 +3,17 @@ import { expect } from "chai";
 import { encoderRLE } from "../src/rle_encoder.mjs"
 
 describe("RLE encoder", () => {
-  test("empty grid", () => {
+    test("empty grid", () => {
     expect(encoderRLE(new Set())).to.deep.equal("!");
   });
 
-  test("single cell", () => {
-      expect(encoderRLE(new Set(["0,0"]))).to.deep.equal("o!");
-    });
+    test("single cell", () => {
+        expect(encoderRLE(new Set(["0,0"]))).to.deep.equal("o!");
+        });
+
+    test("txo cells with same row", () => {
+        expect(encoderRLE(new Set(["0,0","1,0"]))).to.deep.equal("2o!");
+        });
+
+
 });
