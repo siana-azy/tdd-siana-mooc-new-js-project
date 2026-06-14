361a3e5c0cde7a32141f4317117c1b204803fe48
Anais <anaisriviere03@gmail.com>
2026-06-14 16:15:46 +0400

    game of life cells with one neighbour dies (kinda)

--- test/game_of_life.test.mjs
+++ test/game_of_life.test.mjs
@@ -3,16 +3,23 @@ import { expect } from "chai";
 import { gameOfLife } from "../src/game_of_life.mjs";
 
 describe("game of life", () => {
+  //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
   test("alone cells dies ", () => {
-    //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
     const cells  = new Set(["5,5"]);
     const next = gameOfLife(cells);
     expect(next.has("5,5")).to.equal(false);
   });
+  test("cells with one neigbour dies ", () => {
+    const cells  = new Set(["5,5","6,5"]);
+    const next = gameOfLife(cells);
+    expect(next.has("5,5")).to.equal(false);
+  });
+
+  //Any live cell with two or three live neighbours lives on to the next generation.
 });
 
 
-//Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-//Any live cell with two or three live neighbours lives on to the next generation.
+
+
 //Any live cell with more than three live neighbours dies, as if by overpopulation.
 //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.[3][4]: 
