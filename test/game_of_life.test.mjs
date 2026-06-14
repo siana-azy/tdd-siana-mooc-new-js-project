59ebb73db6e0b13a38ec00bfe505698e824b0078
Anais <anaisriviere03@gmail.com>
2026-06-14 11:26:45 +0400

    walking skeleton for block, every src and test is blank in reality

---
+++ test/game_of_life.test.mjs
@@ -0,0 +1,8 @@
+import { describe, test } from "vitest";
+import { expect } from "chai";
+
+describe("game of life", () => {
+  test("Example test", () => {
+    //
+  });
+});
---
+++ test/rle_encoder.test.mjs
@@ -0,0 +1,8 @@
+import { describe, test } from "vitest";
+import { expect } from "chai";
+
+describe("encoder", () => {
+  test("Example test", () => {
+    //
+  });
+});
